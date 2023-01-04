b19b5216c06acd68af29473473c3ef6db6e97cba
youmiya <18516206617@163.com>
2023-01-04 21:48:19 +0800

    preceding-sibling方法和parent方法

--- base/HomeBase.py
+++ base/HomeBase.py
@@ -9,21 +9,31 @@ class HomeBase:
         :return:
         """
         return "//span[contains(@class,'switch')]"
+
     def logo(self):
         """
         进入系统后，首页左上角的logo
         :return:
         """
         return "//div[contains(text(),'二手')]"
-    def welcone(self):
+
+    def welcome(self):
         """
         首页欢迎您回来
         :return:
         """
         return "//span[starts-with(text(),'欢迎您回来')]"
+
     def show_date(self):
         """
         首页显示日期
         :return:
         """
         return "//div[text()='我的日历']/following-sibling::div"
+
+    def home_user_avatar(self):
+        """
+        首页用户头像大图
+        :return:
+        """
+        return "//span[contains(text(),'欢迎您回来')]/parent::div/preceding-sibling::div//img"
