c4bc6307fedf8f005c648d184f8e4a8bd913f6c3
youmiya <18516206617@163.com>
2023-01-06 20:07:27 +0800

    等待页面元素消失/出现

--- base/ObjectMap.py
+++ base/ObjectMap.py
@@ -73,5 +73,65 @@ class ObjectMap:
                 # 如果超时了就break
                 if now_ms >= stop_ms:
                     break
-                time.sleep(0.1)git
+                time.sleep(0.1)
         raise Exception("打开网页时,页面元素在%s秒后任然没有完全加载" % timeout)
+
+    def element_disappear(self, driver, locate_type, locate_expression, timeout=30):
+        """
+        等待页面元素消失
+        :param driver:浏览器驱动
+        :param locate_type: 定位方式类型
+        :param locate_expression: 定位表达式
+        :param timeout: 超时时间
+        :return:
+        """
+        if locate_type:
+            # 开始时间
+            start_ms = time.time()
+            # 设置结束时间
+            stop_ms = start_ms + (timeout * 1000)
+            for x in range(int(timeout * 10)):
+                try:
+                    element = driver.find_element(by=locate_type, value=locate_expression)
+                    if element.is_displayed():
+                        now_ms = time.time() * 1000
+                        if now_ms >= stop_ms:
+                            break
+                        time.sleep(0.1)
+                except Exception:
+                    return True
+            raise Exception("元素没有消失，定位方式：" + locate_type + "\n定位表达式" + locate_expression)
+        else:
+            pass
+
+    def element_appear(self, driver, locate_type, locate_expression, timeout=30):
+        """
+        等待页面元素出现
+        :param driver:浏览器驱动
+        :param locate_type:定位方式
+        :param locate_expression:定位表达式
+        :param timeout:超时时间
+        :return:
+        """
+        if locate_type:
+            # 开始时间
+            start_ms = time.time() * 1000
+            # 设置结束时间
+            stop_ms = start_ms + (timeout * 1000)
+            for x in range(int(timeout * 10)):
+                try:
+                    element = driver.find_element(by=locate_type, value=locate_expression)
+                    if element.is_displayed():
+                        return element
+                    else:
+                        raise Exception()
+                except Exception:
+                    now_ms = time.time() * 1000
+                    if now_ms >= stop_ms:
+                        break
+                    time.sleep(0.1)
+                    pass
+            raise ElementNotVisibleException(
+                "元素没有出现，定位方式：" + locate_type + "\n定位表达式" + locate_expression)
+        else:
+            pass
