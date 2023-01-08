f694c978b93f820e94c4fe8b6ab16b52b814a5a7
youmiya <18516206617@163.com>
2023-01-08 21:08:29 +0800

    补提

--- base/LoginBase.py
+++ base/LoginBase.py
@@ -19,6 +19,6 @@ class LoginBase:
         return "//span[text()='" + button_name + "']/parent::button"
 
 
-if __name__ == '__main__':
-    print(LoginBase().login_input("用户名"))
-    print(LoginBase().login_button("登录"))
+# if __name__ == '__main__':
+#     print(LoginBase().login_input("用户名"))
+#     print(LoginBase().login_button("登录"))
