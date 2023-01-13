658b2fbba5cb5e182a6fd795b518d72097ce9ed3
youmiya <18516206617@163.com>
2023-01-13 17:20:30 +0800

    pytest参数化

--- testcases/test_add_goods.py
+++ testcases/test_add_goods.py
@@ -4,23 +4,50 @@
 
 from time import sleep
 
+import pytest
+
 from page.LoginPage import LoginPage
 from page.LeftMenuPage import LeftMenuPage
 from page.GoodsPage import GoodsPage
 
+goods_info_list = [
+    {
+        "goods_title": "新增批量商品测试book1",
+        "goods_details": "新增批量商品测试详情1",
+        "goods_num": 1,
+        "goods_pic_list": ["商品图片book.jpg"],
+        "goods_price": 100,
+        "goods_status": "上架",
+        "bottom_button_name": "提交"
+    },
+    {
+        "goods_title": "新增批量商品测试book2",
+        "goods_details": "新增批量商品测试详情2",
+        "goods_num": 2,
+        "goods_pic_list": ["商品图片book.jpg"],
+        "goods_price": 100,
+        "goods_status": "上架",
+        "bottom_button_name": "提交"
+    }
+]
+
 
 class TestAddGoods:
-
-    def test_add_goods_001(self, driver):
-        # driver = DriverConfig().driver_config()
+    @pytest.mark.parametrize("goods_info", goods_info_list)
+    def test_add_goods_001(self, driver, goods_info):
         LoginPage().login(driver, "jay")
         sleep(2)
         LeftMenuPage().click_level_one_menu(driver, "产品")
         sleep(1)
         LeftMenuPage().click_level_two_menu(driver, "新增二手商品")
         sleep(2)
-        GoodsPage().add_new_goods(driver, goods_title="新增商品测试book", goods_details="新增商品测试详情", goods_num=1,
-                                  goods_pic_list=["商品图片book.jpg"], goods_price=100, goods_status="上架",
-                                  bottom_button_name="提交")
+        GoodsPage().add_new_goods(
+            driver,
+            goods_title=goods_info["goods_title"],
+            goods_details=goods_info["goods_details"],
+            goods_num=goods_info["goods_num"],
+            goods_pic_list=goods_info["goods_pic_list"],
+            goods_price=goods_info["goods_price"],
+            goods_status=goods_info["goods_status"],
+            bottom_button_name=goods_info["bottom_button_name"])
         sleep(3)
-        # driver.quit()
--- testcases/test_search_order_buy.py
+++ testcases/test_search_order_buy.py
@@ -4,20 +4,23 @@
 
 from time import sleep
 
+import pytest
+
 from page.LoginPage import LoginPage
 from page.OrderPage import OrderPage
 from page.LeftMenuPage import LeftMenuPage
 
+tab_list = ["全部", "待付款", "待发货", "运输中", "待确认", "待评价"]
+
 
 class TestOrderBuy:
-    def test_order_buy(self, driver):
+    @pytest.mark.parametrize("tab", tab_list)
+    def test_order_buy(self, driver, tab):
         LoginPage().login(driver, "jay")
         sleep(2)
         LeftMenuPage().click_level_one_menu(driver, "我的订单")
         sleep(1)
         LeftMenuPage().click_level_two_menu(driver, "已买到的宝贝")
         sleep(1)
-        tab_list = ["全部", "待付款", "待发货", "运输中", "待确认", "待评价"]
-        for tab in tab_list:
-            OrderPage().click_order_tab(driver, tab)
-            sleep(2)
+        OrderPage().click_order_tab(driver, tab)
+        sleep(2)
