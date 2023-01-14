f40b263b13e95e1930b06bb818feba640e58c1ae
youmiya <18516206617@163.com>
2023-01-14 23:31:46 +0800

    图像识别

---
+++ common/find_img.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+# @time: 2023/1/14 23:13
+# Author: zjl
+
+import aircv as ac
+
+from common.tools import get_project_path, sep
+
+
+class FindImg:
+    def img_imread(self, img_path):
+        """
+        读取图片
+        :param img_path:
+        :return:
+        """
+        return ac.imread(img_path)
+
+    def get_confidence(self, source_path, search_path):
+        """
+        查找图片
+        :param source_path: 原图路径
+        :param search_path: 需要查找的图片路径
+        :return:
+        """
+        img_source = self.img_imread(source_path)
+        img_search = self.img_imread(search_path)
+        result = ac.find_template(img_source, img_search)
+        print(result)
+
+
+# if __name__ == '__main__':
+#     source_path = get_project_path() + sep(["img", "img_source.jpg"], add_sep_before=True)
+#     search_path = get_project_path() + sep(["img", "img_search.jpg"], add_sep_before=True)
+#     FindImg().get_confidence(source_path, search_path)
--- testcases/conftest.py
+++ testcases/conftest.py
@@ -2,12 +2,13 @@
 # @time: 2023/1/13 16:38
 # Author: zjl
 
+
 import pytest
 
 from config.driver_config import DriverConfig
 
 
-@pytest.fixture
+@pytest.fixture()
 def driver():
     get_driver = DriverConfig().driver_config()
     yield get_driver
